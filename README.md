8c65d84986b4690570c3fa99d1698d2e9025e3dd
Jared P <jar3dpt@gmail.com>
2020-09-23 14:19:54 -0400

    Fix errors

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ This team is made up of the two Jared in SCI (We think).
 
 ## jared.py
 
-take in name and age and responds with yor birth year
+take in name and age and responds with your birth year
--- jared.py
+++ jared.py
@@ -1,4 +1,4 @@
 name = input("What is your name?")
 age = input("How old are you?")
-year = 2020-age
-print(f"Hello {name}! You were either born in {year}")
+year = 2020-int(age)
+print(f"Hello {name}! You were either born in {year} or {year - 1}")
